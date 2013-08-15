32673fd3379ddb2052c4f694ee25bf785c7f0be3
patscan <patscan@gmail.com>
2013-08-15 00:57:50 -0700

    Boilerplate Rails install code.
    (Using postgres for db; not using test unit.)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jenre::Application.config.secret_token = '4a31b975ee09717c1d5339e65b0f626ece78444770c923f21debcf2199b7201afb2c2841cf7e31f287a0f7f03436a5cf527d7d0638d518939306f68bfebae903'
