9d019c385e1a7b028ace16e7cee2200519490bd5
patscan <patscan@gmail.com>
2013-08-19 21:44:12 -0700

    Add artist attribute to Backbone Song model.

--- app/assets/javascripts/backbone/models/song.js
+++ app/assets/javascripts/backbone/models/song.js
@@ -4,7 +4,8 @@ Jenre.Song = Backbone.Model.extend({
 
   defaults: {
     rdio_id: 0,
-    lyrics: "Sorry, no lyrics were found."
+    lyrics: "Sorry, no lyrics were found.",
+    artist: ""
   }
 });
 
