7e7e28c1b740681182fc9640b6aae014c1b7bb6a
patscan <patscan@gmail.com>
2013-08-19 23:28:48 -0700

    Add title to Backbone Songs Model

--- app/assets/javascripts/backbone/models/song.js
+++ app/assets/javascripts/backbone/models/song.js
@@ -5,7 +5,8 @@ Jenre.Song = Backbone.Model.extend({
   defaults: {
     rdio_id: 0,
     lyrics: "Sorry, no lyrics were found.",
-    artist: ""
+    artist: "",
+    title: ""
   }
 });
 
--- app/assets/javascripts/backbone/models/songs.js
+++ app/assets/javascripts/backbone/models/songs.js
@@ -1,8 +1,8 @@
 var SongsList = Backbone.Collection.extend({
 
-  url: '/songs.json',
+  model: Jenre.Song,
+  url: '/songs.json'
 
-  model: Jenre.Song
 });
 
 Jenre.songs = new SongsList();
